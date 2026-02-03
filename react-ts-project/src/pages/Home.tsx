96e4418339799095d5d07f242d2cf31aae5a73a5
Ankit Mahajan <ankitmahajan2257@gmail.com>
2026-02-03 14:04:57 +0530

    ..

--- react-ts-project/src/pages/Home.tsx
+++ react-ts-project/src/pages/Home.tsx
@@ -1,23 +1,23 @@
 import UserCarad from "../components/UserCard";
 import type { User } from "../interfaces/User";
 
-const Home=()=>{
-    const users:User[]=[
+const Home = () => {
+    const users: User[] = [
         { id: 1, name: "Ankit", email: "ankit@gmail.com" },
         { id: 2, name: "Rahul", email: "rahul@gmail.com" },
-        {id:3,name:"Raj",email:"raj@gmail.com"}
+        { id: 3, name: "Raj", email: "raj@gmail.com" }
     ]
 
-    return(
+    return (
         <div>
             <h2>User List</h2>
             {
-                users.map(user=>(
-                    <UserCarad key={user.id} user={user}/>
+                users.map(user => (
+                    <UserCarad key={user.id} user={user} />
                 ))
             }
         </div>
     )
 }
 
-export default  Home
+export default Home
